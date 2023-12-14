a1e20028cf1524e02d92c74b0ee7831ceba58d88
Carlos LIns <carloslinsalbertoa@gmail.com>
2023-12-14 20:21:51 -0300

    Adição de request de post pra funcionário

--- src/screens/AddColaborator/index.tsx
+++ src/screens/AddColaborator/index.tsx
@@ -11,6 +11,7 @@ import { TextInputProps, ScrollView } from "react-native";
 import Button from "@components/Button";
 import theme from "src/theme";
 import Toast from "@components/Toast";
+import axios from "axios";
 
 interface InputProps extends TextInputProps {}
 
@@ -30,7 +31,16 @@ const Input: FC<InputProps> = ({ style, ...otherProps }) => {
 };
 
 export default function AddColaborator() {
+  // https://unicap-setor-de-protocolos.up.railway.app/funcionario
+
   const [showToast, setShowToast] = useState(false);
+  const [name, setName] = useState("");
+  const [role, setRole] = useState("");
+  const [sector, setSector] = useState("");
+  const [email, setEmail] = useState("");
+  const [birthdate, setBirthdate] = useState("");
+  const [registration, setRegistration] = useState("");
+  const [password, setPassword] = useState("");
 
   const handleShowToast = () => {
     setShowToast(true);
@@ -39,34 +49,58 @@ export default function AddColaborator() {
     }, 2000);
   };
 
+  const handleSubmit = () => {
+    const body = {
+      nome: "Carlos",
+      cargo: "Funcionario protocolo",
+      setor: "protocolo",
+      email: "carloslin@gmail.com",
+      dataNascimento: "2002-12-22T12:00:00",
+      matricula: "87123",
+      senhaHash: "lnkhdas8712",
+    };
+
+    axios
+      .post(
+        "https://unicap-setor-de-protocolos.up.railway.app/funcionario",
+        body
+      )
+      .then((data) => console.log(data))
+      .catch((error) => console.error(error));
+  };
+
   return (
     <ScrollView>
       <Container>
         <Subtitle>Nome</Subtitle>
-        <Input />
+        <Input onChangeText={(value) => setName(value)} />
         <Subtitle>N° de matrícula</Subtitle>
-        <Input />
+        <Input onChangeText={(value) => setRegistration(value)} />
         <Subtitle>E-mail</Subtitle>
-        <Input />
+        <Input onChangeText={(value) => setEmail(value)} />
         <Subtitle>Cargo</Subtitle>
-        <Input />
+        <Input onChangeText={(value) => setRole(value)} />
         <Subtitle>Data de Nascimento</Subtitle>
-        <Input autoComplete="birthdate-day" keyboardType="number-pad" />
+        <Input
+          autoComplete="birthdate-day"
+          keyboardType="number-pad"
+          onChangeText={(value) => setBirthdate(value)}
+        />
         <Subtitle>Setor</Subtitle>
-        <Input />
+        <Input onChangeText={(value) => setSector(value)} />
         <Info>
           <Button
             width={144}
             text="Cadastrar"
             color={theme.color.default}
-            onPress={handleShowToast}
+            onPress={() => handleSubmit()}
           />
           <Button
             width={144}
             text="Cancelar"
             color={theme.color.second}
             onPress={() => {
-              console.log("Fala, Zézé!");
+              handleSubmit();
             }}
           />
         </Info>
